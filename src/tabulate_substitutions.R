769549321c1877817cc9f37b7561d83d0bc9adc3
warnerm <mrwarner09@gmail.com>
2018-12-04 12:41:55 -0500

    first commit

---
+++ src/tabulate_substitutions.R
@@ -0,0 +1,22 @@
+#!/usr/bin/env Rscript
+args = commandArgs(trailingOnly=TRUE)
+
+ann = read.csv(args[1],header=F)
+snps = read.csv(args[2],header=F)
+silR = read.csv(args[3],header=T)
+
+library(plyr)
+
+ann = ann[!duplicated(ann),]
+colnames(ann) = c("CHROM","POS","syn","Gene","transcript")
+snps = snps[!duplicated(snps),]
+colnames(snps) = c("CHROM","POS","fixed")
+df <- merge(ann,snps,by=c("CHROM","POS"))
+summed <- ddply(df,~Gene,summarize,
+                FN = sum(syn=="N" & fixed == "F"),
+                FS = sum(syn=="S" & fixed == "F"),
+                PN = sum(syn=="N" & fixed == "P"),
+                PS = sum(syn=="S" & fixed == "P"))
+summed = merge(summed,silR,by.x="Gene",by.y="gene")
+
+write.csv(summed,file=args[4],row.names = FALSE)
